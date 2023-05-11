5c4bc6680d27e104f8196da675e4639bf905910d
sanhaehong <sanhaehong@outlook.com>
2023-05-11 20:52:51 +0900

    page별 이동 기능 추가

--- src/main/java/com/sanhaehong/project/techview/controller/QuestionController.java
+++ src/main/java/com/sanhaehong/project/techview/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.sanhaehong.project.techview.controller;
 import com.sanhaehong.project.techview.domain.question.Question;
 import com.sanhaehong.project.techview.domain.question.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -21,11 +22,10 @@ public class QuestionController {
 
     @GetMapping("/question/lists")
     public String question(@PageableDefault Pageable pageable, Model model) {
-        System.out.println(questionRepository.findAll(pageable));
-        List<Question> questions = questionRepository.findAll(pageable).stream().toList();
-        System.out.println(questions.size());
-        model.addAttribute("questions", questions);
-        model.addAttribute("count", questionRepository.count());
+        Page<Question> questionPage = questionRepository.findAll(pageable);
+        model.addAttribute("questions", questionPage.stream().toList());
+        model.addAttribute("totalQuestion", questionPage.getTotalElements());
+        model.addAttribute("totalPage", questionPage.getTotalPages());
         return "question";
     }
 
