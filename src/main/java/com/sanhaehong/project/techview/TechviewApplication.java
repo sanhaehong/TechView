e64c6cdfb3a3167bdefb88ee7984a3e105ea1323
sanhaehong <sanhaehong@outlook.com>
2023-05-11 11:11:08 +0900

    Initial commit

---
+++ src/main/java/com/sanhaehong/project/techview/TechviewApplication.java
@@ -0,0 +1,13 @@
+package com.sanhaehong.project.techview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanhaehong/project/techview/TechviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanhaehong.project.techview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
