11be581da87d6874534dcf34cdf7800745862a55
sanhaehong <sanhaehong@outlook.com>
2023-05-11 20:25:41 +0900

    question repository 추가

---
+++ src/main/java/com/sanhaehong/project/techview/domain/question/QuestionRepository.java
@@ -0,0 +1,11 @@
+package com.sanhaehong.project.techview.domain.question;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+}
