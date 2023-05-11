0003bb204760fff6fe2ee44fc7337740c387cc10
sanhaehong <sanhaehong@outlook.com>
2023-05-11 15:50:28 +0900

    login html + controller 추가

---
+++ src/main/java/com/sanhaehong/project/techview/controller/LogInController.java
@@ -0,0 +1,13 @@
+package com.sanhaehong.project.techview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogInController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
