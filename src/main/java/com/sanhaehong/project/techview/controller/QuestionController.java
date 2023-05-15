1c6598d443762a7b4c4555d4813125eca1d44704
sanhaehong <sanhaehong@outlook.com>
2023-05-16 00:28:35 +0900

    질문 상세 조회 기능 추가

--- src/main/java/com/sanhaehong/project/techview/controller/QuestionController.java
+++ src/main/java/com/sanhaehong/project/techview/controller/QuestionController.java
@@ -1,6 +1,13 @@
 package com.sanhaehong.project.techview.controller;
 
+import com.sanhaehong.project.techview.annotation.LogInUser;
+import com.sanhaehong.project.techview.config.security.SessionUser;
+import com.sanhaehong.project.techview.domain.answer.Answer;
+import com.sanhaehong.project.techview.domain.answer.AnswerRepository;
 import com.sanhaehong.project.techview.domain.question.Category;
+import com.sanhaehong.project.techview.domain.user.User;
+import com.sanhaehong.project.techview.domain.user.UserRepository;
+import com.sanhaehong.project.techview.dto.AddAnswerDto;
 import com.sanhaehong.project.techview.dto.AddQuestionDto;
 import com.sanhaehong.project.techview.domain.question.Question;
 import com.sanhaehong.project.techview.domain.question.QuestionRepository;
@@ -14,12 +21,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionRepository questionRepository;
+    private final UserRepository userRepository;
+    private final AnswerRepository answerRepository;
 
     @ModelAttribute("categories")
     public Category[] AddCategoryView() {
@@ -58,5 +71,35 @@ public class QuestionController {
         questionRepository.save(addQuestionDto.toEntity());
         System.out.println("go?");
         return "redirect:/question/lists?page=0";
+    }
+
+    @GetMapping("/question/view/{id}")
+    public String viewQuestion(@PathVariable Long id,
+                               @ModelAttribute("answerForm") AddAnswerDto addAnswerDto,
+                               Model model) {
+        Question question = questionRepository.findById(id).get();
+        List<Answer> answers = question.getAnswers();
+        model.addAttribute("question", question);
+        model.addAttribute("answers", answers);
+        return "question_view";
+    }
+
+    @PostMapping("/question/view/{id}")
+    public String addAnswer(@PathVariable Long id,
+                            @Valid @ModelAttribute("answerForm") AddAnswerDto addAnswerDto,
+                            BindingResult bindingResult,
+                            @LogInUser SessionUser user,
+                            RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()) {
+            return "question_view";
+        }
+        User writer = userRepository.findById(user.getId()).get();
+        Question question = questionRepository.findById(id).get();
+        Answer answer = addAnswerDto.toEntity(question, writer);
+        question.getAnswers().add(answer);
+        answerRepository.save(answer);
+        questionRepository.save(question);
+        redirectAttributes.addAttribute("questionId", id);
+        return "redirect:/question/view/{questionId}";
     }
 }
