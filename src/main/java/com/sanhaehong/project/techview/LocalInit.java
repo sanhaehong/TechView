a4c4ea50719f7cee0fd8bf42bfe65a61813a1b20
sanhaehong <sanhaehong@outlook.com>
2023-05-16 22:56:59 +0900

    Login Test 추가

--- src/main/java/com/sanhaehong/project/techview/LocalInit.java
+++ src/main/java/com/sanhaehong/project/techview/LocalInit.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@Profile("local")
+@Profile({"local", "test"})
 @RequiredArgsConstructor
 public class LocalInit {
 
